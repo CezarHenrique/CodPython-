1fdb84c4de3e40107fe5d39ba0b2886df3b48dd0
Cezar Henrique da Costa e Souza <65462323+CezarHenrique@users.noreply.github.com>
2021-02-01 10:59:29 -0300

    Definir Função
    Programa aonde o usuário define uma função.

---
+++ Definir Funçao.py
@@ -0,0 +1,14 @@
+def Soma (X, Y):
+	R = X +Y
+	return R
+a = int(input("Digite um valor para  a: "))
+b = int(input("Digite um valor para  b: "))
+c = int(input("Digite uma valor para c: "))
+
+s = Soma (a, b)
+print("a + b = {0}" .format(s))
+s = Soma (a, c)
+print("a + c = {0}".format (s))
+s = Soma (b, c)
+print("b + c = {0}".format (s))
+print ("End of Program")
